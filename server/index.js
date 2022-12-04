cffb2ec90d81130b21958bbac875add65c08e09c
nannuflay <mohameddcharafi@gmail.com>
2022-12-04 17:00:27 +0100

    add Routes midlware, create login method

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 
 // Configurations
@@ -38,6 +39,9 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture", register));
+
+//ROUTES
+app.use("/auth", authRoutes);
 // MONGOOSE SETUP
 
 const PORT = process.env.PORT || 6001;
