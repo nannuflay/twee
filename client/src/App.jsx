a6f64b209ed78d41f0807ed8a02ac0a3d87ad132
nannuflay <mohameddcharafi@gmail.com>
2022-12-05 23:23:11 +0100

    setup redux, routes config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<RegisterPage />} path="/register" />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
